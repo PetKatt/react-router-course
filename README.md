f06e9b59424573bd30e05d8b383a594b133a8d8a
Piotr Wiercinski <piotrwier@o2.pl>
2018-03-12 00:23:40 +0100

    basic branch: Basic example DONE

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-This code is written based on TylerMcGinnis.com's React Router course - (https://tylermcginnis.com/react-router-philosophy-introduction/)
+This code is written based on TylerMcGinnis.com's React Router course - (https://tylermcginnis.com/react-router-philosophy-introduction/).
+
+It covers the examples from [React Training/React Router](https://reacttraining.com/react-router/web/example). Each branch has its own example.
+
+Check it out!
+
+1. [Basic](https://reacttraining.com/react-router/web/example/basic) 
 
 
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 
 import './App.css';
+
+const Home = () => (
+	<div>
+		<h2>Home</h2>
+	</div>
+);
+
+const About = () => (
+	<div>
+		<h2>About</h2>
+	</div>
+);
+
+const Topics = ({ match }) => (
+	<div>
+		<h2>Topics</h2>
+		<ul>
+			<li>
+				<Link to={`${match.url}/rendering`}>Rendering with React</Link>
+			</li>
+			<li>
+				<Link to={`${match.url}/components`}>Components</Link>
+			</li>
+			<li>
+				<Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+			</li>
+		</ul>
+
+		<Route path="/topics/:topicID" component={Topic} />
+	</div>
+);
+
+const Topic = ({match}) => (
+	<h3>{match.params.topicID}</h3>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+    	<Router>
+	      <div className="App">
+	        <ul>
+	        	<li><Link to="/">Home</Link></li>
+	        	<li><Link to="/about">About</Link></li>
+	        	<li><Link to="/topics">Topics</Link></li>
+	        </ul>
+
+	        <hr />
+
+	        <Route exact path="/" component={Home} />
+	        <Route path="/about" component={About} />
+	        <Route path="/topics" component={Topics} />
+	        <Route exact path="/topics" render={() => (
+	        	<p>Please choose a topic above</p>
+	        )} />
+	      </div>
+	    </Router>
     );
   }
 }
