2eabc5ce8920e31d85728ad63ba97817d62ad9f9
Piotr Wiercinski <piotrwier@o2.pl>
2018-03-15 08:07:01 +0100

    redirects branch: login interface DONE

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-This code is written based on TylerMcGinnis.com's React Router course - (https://tylermcginnis.com/react-router-philosophy-introduction/)
+This code is written based on TylerMcGinnis.com's React Router course - (https://tylermcginnis.com/react-router-philosophy-introduction/).
 
+It covers the examples from [React Training/React Router](https://reacttraining.com/react-router/web/example). Each branch has its own example.
+
+Check it out!
+
+1. [Basic](https://reacttraining.com/react-router/web/example/basic) 
+2. [URL Parameters](https://reacttraining.com/react-router/web/example/url-params)
+3. [Redirects (Auth)](https://reacttraining.com/react-router/web/example/auth-workflow)
+ 	 
 
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import {
 	BrowserRouter as Router,
 	Route,
 	Link,
-	Redirect
+	Redirect,
+	withRouter
 } from "react-router-dom";
 
 import './App.css';
@@ -22,7 +23,7 @@ const fakeAuth = {
 };
 
 const Public = () => <h3>Public</h3>;
-const Protected = () => <h3>Protected</h3>;
+const Protected = (props) => console.log("PROTECTED", props) || <h3>Protected</h3>;
 
 class Login extends Component {
 	state = {
@@ -30,24 +31,27 @@ class Login extends Component {
 	}
 
 	login = () => {
+		console.log("LOGIN", this.props);
 		fakeAuth.authenticate(() => {
 			this.setState(() => ({
 				redirectToReferrer: true
 			}));
 		});
 	}
+
 	render() {
 		const { redirectToReferrer } = this.state; // NEW SYNTAX!!!
+		const { from } = this.props.location.state || { from: { pathname: "/" } };
 
 		if (redirectToReferrer === true) {
 			return(
-				<Redirect to="/" />
+				<Redirect to={ from } />
 			);
 		}
 
 		return(
 			<div>
-				<p>You must to log in!</p>
+				<p>You must to log in to view this page at {from.pathname}!</p>
 				<button onClick={this.login}>Log in</button>
 			</div>
 		);
@@ -60,17 +64,35 @@ const PrivateRoute = ({ component: Component, ...rest}) => {
 			return(
 				fakeAuth.isAuthenticated === true 
 					? <Component {...props} /> 
-					: <Redirect to="/login" />
+				/*: <Redirect to="/login" */ 
+					: <Redirect to={{
+						pathname: "/login",
+						state: { from: props.location }
+					}} />
 			);
 		}} />
 	);
 };
+
+const AuthButton = withRouter(({ history }) => 
+	console.log("AuthButton:HISTORY Object", history) || 
+	(
+		fakeAuth.isAuthenticated === true
+			? <p>
+					Welcome! <button onClick={() => {
+						fakeAuth.signout(() => history.push("/"));
+					}}>Sign Out</button>
+				</p>
+			: <p>You are not logged in.</p>
+	)
+);
 
 class App extends Component {
   render() {
     return (
     	<Router>
 	      <div className="App">
+	      	<AuthButton />
 					<ul>
 						<li><Link to="/public">Public Page</Link></li>
 						<li><Link to="/protected">Protected Page</Link></li>
