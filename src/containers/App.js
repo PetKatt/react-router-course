55078c119f853512f378dff367210663c60c6627
Piotr Wiercinski <piotrwier@o2.pl>
2018-03-15 15:08:05 +0100

    preventTransitions: basic logic(prompt) setup

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,17 +2,56 @@ import React, { Component } from 'react';
 import {
 	BrowserRouter as Router,
 	Route,
-	Link
+	Link,
+	Prompt
 } from "react-router-dom";
 
 import './App.css';
+
+class Form extends Component {
+	state = {
+		isBlocking: false
+	}
+
+	render() {
+		const { isBlocking } = this.state;
+
+		return (
+			<form>
+				FORM
+				<Prompt
+					when={isBlocking}
+					message={(location) => `Are you sure you want to go to ${location.pathname}?`}
+				/>
+
+				<p>
+					Blocking?
+					{isBlocking === true
+						? "Yes, click a link or the back button."
+						: "Nope."
+					}
+				</p>
+
+
+			</form>
+		);
+	}
+}
 
 class App extends Component {
   render() {
     return (
     	<Router>
 	      <div className="App">
-	      	
+	      	<ul>
+	      		<li><Link to="/">Form</Link></li>
+	      		<li><Link to="/one">One</Link></li>
+	      		<li><Link to="/two">Two</Link></li>
+	      	</ul>
+
+	      	<Route path="/" exact component={Form} />
+	      	<Route path="/one" render={() => <h3>One</h3>} />
+	      	<Route path="/two" render={() => <h3>Two</h3>} />
 	      </div>
 	    </Router>
     );
