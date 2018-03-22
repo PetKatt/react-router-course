a8de0b43bdd37d93701ffc73b7d122525b4384a2
Piotr Wiercinski <piotrwier@o2.pl>
2018-03-22 07:10:39 +0100

    404page branch: setup ready

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,82 +3,27 @@ import {
 	BrowserRouter as Router,
 	Route,
 	Link,
-	Prompt
+	Switch,
+	Redirect
 } from "react-router-dom";
 
 import './App.css';
 
-class Form extends Component {
-	state = {
-		isBlocking: false
-	}
+const Home = () => <h1>Home</h1>;
+const WillMatch = () => <h1>Matched!</h1>;
 
-	render() {
-		const { isBlocking } = this.state;
-
-		return (
-			<form onSubmit={(event) => {
-				event.preventDefault();
-				event.target.reset();
-				this.setState(() => ({
-					isBlocking: false
-				}))
-			}}>
-				FORM
-				<Prompt
-					when={isBlocking}
-					message={(location) => `Are you sure you want to go to ${location.pathname}?`}
-				/>
-
-				<p>
-					Blocking?
-					{isBlocking === true
-						? "Yes, click a link or the back button."
-						: "Nope."
-					}
-				</p>
-
-				<p>
-					<input
-						size="50"
-						placeholder="type something to block transition"
-						onChange={(event) => {
-							const isBlocking = event.target.value.length > 0;
-							this.setState(() => ({
-								isBlocking
-							}));
-						}}
-					/>
-				</p>
-
-				<p>
-					<button>
-						Submit to stop blocking
-					</button>
-				</p>
-
-
-
-
-			</form>
-		);
-	}
-}
+const NoMatch = ({ location }) => (
+	<div>
+		<h3>No match for <code>{ location.pathname }</code></h3>
+	</div>
+);
 
 class App extends Component {
   render() {
     return (
     	<Router>
 	      <div className="App">
-	      	<ul>
-	      		<li><Link to="/">Form</Link></li>
-	      		<li><Link to="/one">One</Link></li>
-	      		<li><Link to="/two">Two</Link></li>
-	      	</ul>
-
-	      	<Route path="/" exact component={Form} />
-	      	<Route path="/one" render={() => <h3>One</h3>} />
-	      	<Route path="/two" render={() => <h3>Two</h3>} />
+	      	
 	      </div>
 	    </Router>
     );
