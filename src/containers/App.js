68cde83da5fe6908a2df3a7bee588b9ca31a7e78
Piotr Wiercinski <piotrwier@o2.pl>
2018-03-23 10:20:59 +0100

    ambiguousMatches branch: Switch DONE

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,128 +3,40 @@ import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
-	Link,
-	Redirect
+	Link
 } from "react-router-dom";
-import {
-	TransitionGroup,
-	CSSTransition
-} from "react-transition-group";
 
 import './App.css';
+
+const About = () => <h2>About</h2>;
+const Company = () => <h2>Company</h2>;
+const User = ({ match }) => (
+	<div>
+		<h2>User: {match.params.user}</h2>
+	</div>
+);
 
 class App extends Component {
   render() {
     return (
     	<Router>
-    		<Route render={({ location }) => console.log(location) || (
-    			<div style={styles.fill}>
-		      	<Route path="/" exact render={() => (
-		      		<Redirect to="/hsl/10/90/50" />
-		      	)} />
-
-		      	<ul style={styles.nav}>
-		      		<NavLink to="/hsl/10/90/50">Red</NavLink>
-		      		<NavLink to="/hsl/120/100/40">Green</NavLink>
-		      		<NavLink to="/rgb/33/150/243">Blue</NavLink>
-		      		<NavLink to="/rgb/240/98/146">Pink</NavLink>
-		      	</ul>
-
-		      	<div style={styles.content}>
-		      		<TransitionGroup>
-		      			<CSSTransition key={location.key} timeout={300} classNames="fade">
-				      		<Switch location={location}>
-					      		<Route path="/hsl/:h/:s/:l" component={HSL} />
-					      		<Route path="/rgb/:r/:g/:b" component={RGB} />
-					      		<Route render={() => <div>Not Found ;/</div>} />
-					      	</Switch>
-					      </CSSTransition>
-				      </TransitionGroup>
-		      	</div>
-		      </div>
-    		)} />   
-	    </Router>
+    		<div>
+    			<ul>
+    				<li><Link to="/about">About</Link></li>
+    				<li><Link to="/company">Company</Link></li>
+    				<li><Link to="/kim">Kim</Link></li>
+    				<li><Link to="/chris">Chris</Link></li>
+    			</ul>
+    			
+    			<Switch>
+	    			<Route path="/about" component={About} />
+	    			<Route path="/company" component={Company} />
+	    			<Route path="/:user" component={User} />
+	    		</Switch>
+    		</div>
+    	</Router>
     );
   }
 }
-
-const HSL = ({ match }) => {
-	const { params } = match;
-
-	return (
-		<div style={{
-			...styles.hsl,
-			background: `hsl(${params.h}, ${params.s}%, ${params.l}%)`
-		}}>
-			hsl({params.h}, {params.s}%, {params.l}%)
-		</div>
-	);
-};
-
-const RGB = ({ match }) => {
-	const { params } = match;
-
-	return (
-		<div style={{
-			...styles.rgb,
-			background: `rgb(${params.r}, ${params.g}, ${params.b})`
-		}}>
-			rgb({params.r}, {params.g}, {params.b})
-		</div>
-	);
-};
-
-const NavLink = (props) => (
-	<li style={styles.navItem}>
-		<Link {...props} style={{ color: "inherit" }} />
-	</li>
-);
-
-const styles = {};
-
-styles.fill = {
-	position: "absolute",
-	left: 0,
-	right: 0,
-	top: 0,
-	bottom: 0
-};
-
-styles.content = {
-	...styles.fill,
-	top: "40px",
-	textAlign: "center"
-};
-
-styles.nav = {
-	padding: 0,
-	margin: 0,
-	position: "absolute",
-	top: 0,
-	height: "40px",
-	width: "100%",
-	display: "flex"
-};
-
-styles.navItem = {
-	textAlign: "center",
-	flex: 1,
-	listStyleType: "none",
-	padding: "10px"
-};
-
-styles.hsl = {
-	...styles.fill,
-	color: "white",
-	paddingTop: "20px",
-	fontSize: "30px"
-};
-
-styles.rgb = {
-	...styles.fill,
-	color: "white",
-	paddingTop: "20px",
-	fontSize: "30px"
-};
 
 export default App;
